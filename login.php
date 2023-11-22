6e5aa6f19c3a915618f89e037653d79e8edd2a9d
seifxmx <151448421+seifxmx@users.noreply.github.com>
2023-11-22 13:13:22 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+  <meta charset="UTF-8">
+  <title>عنوان الصفحة</title>
+</head>
+<body>
+
+  <?php
+    // هذا هو الكود PHP
+    echo "مرحبًا بالعالم!";
+  ?>
+
+</body>
+</html>
